c743a814879fe6327a38d66f7bc8f0ad69da119e
BogdanGlazkov <bglazkov@i.ua>
2023-06-24 21:57:11 +0300

    Add metadata for different pages

--- src/app/post/[...postId]/page.tsx
+++ src/app/post/[...postId]/page.tsx
@@ -1,7 +1,18 @@
-import { FC } from "react";
+import { Metadata } from "next";
+import { Post } from "../../../types";
 
 interface PageProps {
   params: { postId: string };
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = (await res.json()) as Post;
+  return { title: data.title };
 }
 
 const page = async ({ params }: PageProps) => {
